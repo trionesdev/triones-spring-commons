078b9817705d30b91e190cda2e76b1e4595f5694
Bane.Shi <fengxiaotx@163.com>
2024-08-07 11:22:49 +0800

    feat: add actor context attributes

--- triones-spring-security/src/main/java/com/trionesdev/spring/security/jwt/JwtAuthenticationFilter.java
+++ triones-spring-security/src/main/java/com/trionesdev/spring/security/jwt/JwtAuthenticationFilter.java
@@ -85,12 +85,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     String role = Optional.ofNullable(claims.get(ACTOR_ROLE)).map(String::valueOf).orElse(null);
                     String tenantId = Optional.ofNullable(claims.get(ACTOR_TENANT_ID)).map(String::valueOf).orElse(null);
                     String tenantMemberId = Optional.ofNullable(claims.get(ACTOR_TENANT_MEMBER_ID)).map(String::valueOf).orElse(null);
+                    Map<String,Object> attributes = Optional.ofNullable(claims.get(ACTOR_ATTRIBUTES))
+                            .map(o -> JSON.parseObject(JSON.toJSONString(o), new TypeReference<Map<String, Object>>() {
+                            })).orElse(null);
                     if ((Objects.nonNull(actorId) || Objects.nonNull(userId)) && Objects.nonNull(role)) {
                         actor.setActorId(actorId);
                         actor.setUserId(userId);
                         actor.setRole(role);
                         actor.setTenantId(tenantId);
                         actor.setTenantMemberId(tenantMemberId);
+                        actor.setAttributes(attributes);
                         actor.setTime(Instant.now());
                         JwtUserDetails userDetails =
                                 JwtUserDetails.builder().token(authorization)
@@ -99,6 +103,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                         .role(actor.getRole())
                                         .tenantId(actor.getTenantId())
                                         .tenantMemberId(actor.getTenantMemberId())
+                                        .attributes(attributes)
                                         .build();
                         List<SimpleGrantedAuthority> authorities = Lists.newArrayList(new SimpleGrantedAuthority(role));
                         Authentication authentication =
