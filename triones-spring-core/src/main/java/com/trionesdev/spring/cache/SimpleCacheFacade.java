cd192afccbcbfe5920093d330f98f69077faf669
bane.shi <fengxiaotx@163.com>
2024-12-09 22:51:55 +0800

    feat: cache

--- triones-spring-core/src/main/java/com/trionesdev/spring/cache/SimpleCacheFacade.java
+++ triones-spring-core/src/main/java/com/trionesdev/spring/cache/SimpleCacheFacade.java
@@ -2,30 +2,34 @@ package com.trionesdev.spring.cache;
 
 import org.springframework.cache.CacheManager;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleCacheFacade<K, V> extends AbstractCacheFacade<K, V> {
+    private final ConcurrentMap<K, V> cache = new ConcurrentHashMap<>();
+
     public SimpleCacheFacade(CacheManager cacheManager) {
         super(cacheManager);
     }
 
     @Override
     public void put(K key, V value, long timeout, TimeUnit unit) {
-
+        cache.put(key, value);
     }
 
     @Override
     public void put(K key, V value) {
-
+        cache.put(key, value);
     }
 
     @Override
     public V get(K key) {
-        return null;
+        return cache.get(key);
     }
 
     @Override
     public void evict(K key) {
-
+        cache.remove(key);
     }
 }
