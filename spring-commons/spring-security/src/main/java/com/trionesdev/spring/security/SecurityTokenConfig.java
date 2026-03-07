478bc4fa7558c101b7eff9e7a533065853e7a781
bane.shi <fengxiaotx@163.com>
2026-03-07 16:15:15 +0800

    feat:

--- spring-commons/spring-security/src/main/java/com/trionesdev/spring/security/SecurityTokenConfig.java
+++ spring-commons/spring-security/src/main/java/com/trionesdev/spring/security/SecurityTokenConfig.java
@@ -1,11 +1,22 @@
 package com.trionesdev.spring.security;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SecurityTokenConfig {
+    @Builder.Default
     private String tokenKey = "token";
+    @Builder.Default
     private String secret = "trionesdev_secret";
+    @Builder.Default
     private int expires = 86400;
+    @Builder.Default
     private int refreshExpires = 2592000;
 }
